a18f317c2a02093e7c573b7a8f62008e91a00fe4
Tolotra <tolotor@yahoo.fr>
2023-01-27 22:25:23 +0300

    Path aliases setup with craco

---
+++ craco.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@configs': path.resolve(__dirname, 'src/configs'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@hooks': path.resolve(__dirname, 'src/helpers'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@redux': path.resolve(__dirname, 'src/redux'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
+  },
+};
