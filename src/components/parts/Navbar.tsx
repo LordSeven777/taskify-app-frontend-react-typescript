28568db98c7c085a0dfdc430b18fc0006c003ed2
Tolotra <tolotor@yahoo.fr>
2023-02-06 23:53:32 +0300

    Update navbar links based on auth status

--- src/components/parts/Navbar.tsx
+++ src/components/parts/Navbar.tsx
@@ -1,8 +1,15 @@
+import { useSelector } from 'react-redux';
 import { NavLink, Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+// Store
+import type { RootState } from '@/store';
 
 export default function AppNavbar() {
+  const { isAuthenticated } = useSelector((state: RootState) => state.auth);
+
   return (
     <Navbar
       bg="primary"
@@ -19,12 +26,29 @@ export default function AppNavbar() {
         <Navbar.Toggle aria-controls="app-navbar" />
         <Navbar.Collapse id="app-navbar">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/login">
-              Sign in
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/register">
-              Register
-            </Nav.Link>
+            {!isAuthenticated && (
+              <>
+                <Nav.Link as={NavLink} to="/login">
+                  Sign in
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
+            {isAuthenticated && (
+              <>
+                <Nav.Link as={NavLink} to="/tasks">
+                  Tasks
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/register">
+                  My account
+                </Nav.Link>
+                <Nav.Link as={Button} variant="light" className="text-dark">
+                  Log out
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </div>
