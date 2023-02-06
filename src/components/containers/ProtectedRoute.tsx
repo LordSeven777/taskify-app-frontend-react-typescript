3bc4e5b536f8dd643ca36f84f598230340e28966
Tolotra <tolotor@yahoo.fr>
2023-02-06 23:39:14 +0300

    Fix protected route redirection

--- src/components/containers/ProtectedRoute.tsx
+++ src/components/containers/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { useRef, useEffect, PropsWithChildren } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -21,26 +21,32 @@ export default function ProtectedRoute({
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
   const dispatch = useDispatch();
 
+  const renderCountRef = useRef(0);
+
   const isAuthorized = reverse ? !isAuthenticated : isAuthenticated;
 
-  if (!isAuthorized) {
-    const message = reverse
-      ? 'You cannot access that route while being logged in'
-      : 'You need to sign in in order to access that route';
-    const redirectTo = reverse ? '/tasks' : '/login';
-    dispatch(
-      addFeedback({
-        reason: 'browse',
-        data: {
-          type: 'error',
-          title: 'Access denied',
-          message,
-          duration: 3500,
-        },
-      })
-    );
-    navigate(redirectTo);
-  }
+  useEffect(() => {
+    if (renderCountRef.current === 0) {
+      const message = reverse
+        ? 'You cannot visit that page while being logged in'
+        : 'You need to sign in in order to visit that page';
+      const redirectTo = reverse ? '/tasks' : '/login';
+      dispatch(
+        addFeedback({
+          reason: 'browse',
+          data: {
+            type: 'error',
+            title: 'Access denied',
+            message,
+            duration: 3500,
+          },
+        })
+      );
+      navigate(redirectTo);
+    }
+    renderCountRef.current++;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <>{children}</>;
+  return isAuthorized ? <>{children}</> : null;
 }
