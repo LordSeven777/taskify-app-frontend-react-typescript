6262be2aee32a3d448748f7fd9dfc81b794a8ddc
Tolotra <tolotor@yahoo.fr>
2023-02-12 10:25:20 +0300

    Fix get strict iso date util

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -7,8 +7,8 @@ export function getStrictDateISO(date: Date): string {
   return (
     date.getFullYear() +
     '-' +
-    `${date.getMonth() + 1}`.padStart(2, '0') +
+    (date.getMonth() + 1).toString().padStart(2, '0') +
     '-' +
-    `${date.getDate()}`.padStart(2, '0')
+    date.getDate().toString().padStart(2, '0')
   );
 }
