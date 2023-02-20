b8c8cf8e23a558835f9202985d2a4e9d094754e8
Tolotra <tolotor@yahoo.fr>
2023-02-20 11:02:11 +0300

    Tasks types

---
+++ src/types/Task.d.ts
@@ -0,0 +1,20 @@
+import type { UserAttributes } from './user';
+import type { LabelAttributes } from './Label';
+import type { Timestamps } from './timestamps';
+
+export interface TaskAttributes extends Timestamps {
+  _id: string;
+  name: string;
+  description?: string;
+  checkList: string[];
+  startsAt: string;
+  endsAt: string;
+  isCompleted: boolean;
+  user: UserAttributes;
+  labels: LabelAttributes[];
+}
+
+export type PartialTaskAttributes = Omit<
+  TaskAttributes,
+  'description' | 'checklist'
+>;
